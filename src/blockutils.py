49813247e1d61af2e074154f017a38f3eae7f1ba
marvel_magnum <marvel_magnum@yahoo.com>
2024-08-21 13:32:29 -0300

    added block identifier logic

--- src/blockutils.py
+++ src/blockutils.py
@@ -1,4 +1,56 @@
+import re
 
+block_type_heading = "heading"
+block_type_paragraph = "paragraph"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered list"
+block_type_ordered_list = "ordered_list"
+
+# markdown blob to separate blocks
 def markdown_to_blocks(markdown):
     result = list(filter(lambda x: x, list(map(lambda x: x.strip(), markdown.split("\n\n")))))
     return result
+
+def __is_heading___(block):
+    return len(re.findall(r"^#{1,6} ", block)) > 0
+
+def __is_code___(block):
+    code_mark = "```"
+    return block[:3] == code_mark and block[-3:] == code_mark
+
+def __is_quote___(block):
+    lines = block.split('\n')
+    matches = list(filter(lambda x: x[0] == '>', lines))
+    return len(matches) == len(lines)
+
+def __is__unordered_list__(block):
+    lines = block.split('\n')
+    matches = list(filter(lambda x: x[0:2] == '- ' or x[0:2] == '* ', lines))
+    return len(matches) == len(lines)
+
+def __is__ordered_list__(block):
+    lines = block.split('\n')
+    counter = 1
+    for line in lines:
+        if line[1:3] == '. ' and line[0] == str(counter):
+            counter += 1
+            continue
+        else:
+            return False
+    return True
+
+# identify block type
+def block_to_block_type(block):
+    if __is_heading___(block):
+        return block_type_heading
+    elif __is_code___(block):
+        return block_type_code
+    elif __is_quote___(block):
+        return block_type_quote
+    elif __is__unordered_list__(block):
+        return block_type_unordered_list
+    elif __is__ordered_list__(block):
+        return block_type_ordered_list
+    else:
+        return block_type_paragraph
--- src/test_blockutils.py
+++ src/test_blockutils.py
@@ -47,3 +47,195 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         self.assertEqual(result[0], '# This is a heading')
         self.assertEqual(result[1], 'This is a paragraph of text. It has some **bold** and *italic* words inside of it.')
         self.assertEqual(result[2], '* This is the first list item in a list block\n* This is a list item\n* This is another list item')
+
+
+class TestBlockIdentifiers(unittest.TestCase):
+    def test_heading1(self):
+        text = "## This is a heading"
+        self.assertEqual(blockutils.__is_heading___(text), True)
+
+    def test_heading2(self):
+        text = "####### This is not a heading"
+        self.assertEqual(blockutils.__is_heading___(text), False)
+
+    def test_heading2(self):
+        text = "#This is not a heading"
+        self.assertEqual(blockutils.__is_heading___(text), False) 
+
+    def test_heading2(self):
+        text = "This is # not a heading"
+        self.assertEqual(blockutils.__is_heading___(text), False) 
+
+    def test_code1(self):
+        text = "```This is a code block```"
+        self.assertEqual(blockutils.__is_code___(text), True)
+
+    def test_code2(self):
+        text = """```This is a code block
+        still continues```"""
+        self.assertEqual(blockutils.__is_code___(text), True)
+
+    def test_code3(self):
+        text = """``This is a not code block
+        still continues```"""
+        self.assertEqual(blockutils.__is_code___(text), False)
+
+    def test_code4(self):
+        text = """```This is a not code block
+        still continues`"""
+        self.assertEqual(blockutils.__is_code___(text), False)
+
+    def test_code5(self):
+        text = """This is a ```not``` code block
+        still continues"""
+        self.assertEqual(blockutils.__is_code___(text), False)
+
+    def test_quote1(self):
+        text = "> This is a quote block"
+        self.assertEqual(blockutils.__is_quote___(text), True)
+
+    def test_quote2(self):
+        text = """>This is a quote block
+> still continues"""
+        self.assertEqual(blockutils.__is_quote___(text), True)
+
+    def test_quote3(self):
+        text = """>> This is a quote block
+> still continues"""
+        self.assertEqual(blockutils.__is_quote___(text), True)
+
+    def test_quote4(self):
+        text = """>This is a quote block
+>>still continues`"""
+        self.assertEqual(blockutils.__is_quote___(text), True)
+
+    def test_quote5(self):
+        text = """This is a > not quote block
+>still continues"""
+        self.assertEqual(blockutils.__is_quote___(text), False)
+
+    def test_uo_list1(self):
+        text = """* list item1
+- list item2
+- list item3
+* list item4"""
+        self.assertEqual(blockutils.__is__unordered_list__(text), True)
+
+    def test_uo_list2(self):
+        text = """* list item1
+-list item2
+- list item3
+* list item4"""
+        self.assertEqual(blockutils.__is__unordered_list__(text), False)
+
+    def test_uo_list3(self):
+        text = """* list item1
+- list item2
+list item3
+* list item4"""
+        self.assertEqual(blockutils.__is__unordered_list__(text), False) 
+
+    def test_uo_list4(self):
+        text = """* list item1
+* list item2
+* list item3
+* list item4"""
+        self.assertEqual(blockutils.__is__unordered_list__(text), True)
+
+    def test_uo_list5(self):
+        text = """* list item1
+* list item2
+* list - item3
+* list item4"""
+        self.assertEqual(blockutils.__is__unordered_list__(text), True)
+
+    def test_uo_list6(self):
+        text = """- list item1
+- list item2
+list - item3
+- list item4"""
+        self.assertEqual(blockutils.__is__unordered_list__(text), False)
+
+    def test_o_list1(self):
+        text = """1. list item1
+2. list item2
+3. list item3
+4. list item4"""
+        self.assertEqual(blockutils.__is__ordered_list__(text), True)
+
+    def test_o_list2(self):
+        text = """1. list item1
+2.list item2
+3. list item3
+4. list item4"""
+        self.assertEqual(blockutils.__is__ordered_list__(text), False)
+
+    def test_o_list3(self):
+        text = """1. list item1
+2. list item2
+list item3
+3. list item4"""
+        self.assertEqual(blockutils.__is__ordered_list__(text), False)
+
+    def test_o_list4(self):
+        text = """1 list item1
+2 list item2
+3 list item3
+4 list item4"""
+        self.assertEqual(blockutils.__is__ordered_list__(text), False)
+
+    def test_o_list5(self):
+        text = """1. list item1
+2. list item2
+3. list 3. item3
+4. list item4"""
+        self.assertEqual(blockutils.__is__ordered_list__(text), True)
+
+    def test_o_list6(self):
+        text = """1. list item1
+2. list item2
+list 3. item3
+4. list item4"""
+        self.assertEqual(blockutils.__is__ordered_list__(text), False)
+
+    def test_o_list7(self):
+        text = """1. list item1
+2. list item2
+2. list item3
+3. list item4"""
+        self.assertEqual(blockutils.__is__ordered_list__(text), False)
+
+
+class TestBlockToType(unittest.TestCase):
+    def test_heading(self):
+        text = "# This is a heading"
+        self.assertEqual(blockutils.block_to_block_type(text), blockutils.block_type_heading)
+
+    def test_code(self):
+        text = """```This is a code block
+        still continues```"""
+        self.assertEqual(blockutils.block_to_block_type(text), blockutils.block_type_code)
+
+    def test_quote(self):
+        text = """>This is a quote block
+> still continues"""
+        self.assertEqual(blockutils.block_to_block_type(text), blockutils.block_type_quote)
+
+    def test_unordered_list(self):
+        text = """- list item1
+- list item2
+- list item3
+- list item4"""
+        self.assertEqual(blockutils.block_to_block_type(text), blockutils.block_type_unordered_list)
+
+    def test_ordered_list(self):
+        text = """1. list item1
+2. list item2
+3. list item3
+4. list item4"""
+        self.assertEqual(blockutils.block_to_block_type(text), blockutils.block_type_ordered_list)
+
+    def test_paragraph(self):
+        text = """This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+More blah blah text here."""
+        self.assertEqual(blockutils.block_to_block_type(text), blockutils.block_type_paragraph)
