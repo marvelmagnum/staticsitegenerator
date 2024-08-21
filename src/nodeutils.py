728b4e0e6ac5b98c8e5465475a0b82631c54784e
marvel_magnum <marvel_magnum@yahoo.com>
2024-08-21 00:00:48 -0300

    added image and link extraction

--- src/nodeutils.py
+++ src/nodeutils.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode
 
 def split_textnode_delimiter(input_nodes, delimiter, text_type):
@@ -21,8 +22,70 @@ def split_textnode_delimiter(input_nodes, delimiter, text_type):
                     is_inline = True
                 continue
 
-            work_lst.append(TextNode(f'"{chunks[i]}"', f'"{text_type}"' if is_inline else f'"text"'))
+            work_lst.append(TextNode(chunks[i], text_type if is_inline else "text"))
             is_inline = not is_inline
 
         result += work_lst
     return result
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+def __node_text_to_inline_type__(node_text, matches, token_template, type):
+    result = []
+    scan_text = node_text
+    token_part1 = token_template.split("item1",1)
+    token_part23 = token_part1[1].split("item2",1)
+    tokens = [token_part1[0]] + token_part23
+    
+    for match in matches:
+        token = f"{tokens[0]}{match[0]}{tokens[1]}{match[1]}{tokens[2]}"
+        chunks = scan_text.split(token, 1)
+        if chunks[0]:   # create nodes for non-empty chunks
+            result.append(TextNode(chunks[0], "text"))
+        result.append(TextNode(match[0], type, match[1]))
+        if len(chunks) == 2:
+            scan_text = chunks[1]
+    return result
+
+def split_nodes_image(input_nodes):
+    result = []
+    for node in input_nodes:
+        if not isinstance(node, TextNode):
+            raise ValueError("Only TextNode type input is allowed")
+        
+        if node.text_type != 'text':    # only text type is process. Other textnode types are unaffected.
+            result.append(node)
+
+        matches = extract_markdown_images(node.text)
+
+        if not matches:     # nodes without image links are unaffected
+            result.append(node)
+
+        result += __node_text_to_inline_type__(node.text, matches, f"![item1](item2)", "image")
+    return result
+
+def split_nodes_link(input_nodes):
+    result = []
+    for node in input_nodes:
+        if not isinstance(node, TextNode):
+            raise ValueError("Only TextNode type input is allowed")
+        
+        if node.text_type != 'text':    # only text type is process. Other textnode types are unaffected.
+            result.append(node)
+
+        matches = extract_markdown_links(node.text)
+
+        if not matches:     # nodes without image links are unaffected
+            result.append(node)
+
+        
+        result += __node_text_to_inline_type__(node.text, matches, f"[item1](item2)", "link")
+    return result
+
+node = TextNode("This is text with a and", "text")
+result = split_nodes_image([node])
+print(result)
--- src/test_nodeutils.py
+++ src/test_nodeutils.py
@@ -7,28 +7,28 @@ class TestNodeUtils(unittest.TestCase):
     def test_start(self):
         node = TextNode("**This** is text with a code block word", "text")
         new_nodes = nodeutils.split_textnode_delimiter([node], "**", "bold")
-        self.assertEqual(new_nodes[0].text, '"This"')
-        self.assertEqual(new_nodes[0].text_type, '"bold"')
-        self.assertEqual(new_nodes[1].text, '" is text with a code block word"')
-        self.assertEqual(new_nodes[1].text_type, '"text"')
+        self.assertEqual(new_nodes[0].text, "This")
+        self.assertEqual(new_nodes[0].text_type, "bold")
+        self.assertEqual(new_nodes[1].text, " is text with a code block word")
+        self.assertEqual(new_nodes[1].text_type, "text")
 
     def test_middle(self):
         node = TextNode("This is text with a `code block` word", "text")
         new_nodes = nodeutils.split_textnode_delimiter([node], "`", "code")
-        self.assertEqual(new_nodes[0].text, '"This is text with a "')
-        self.assertEqual(new_nodes[0].text_type, '"text"')
-        self.assertEqual(new_nodes[1].text, '"code block"')
-        self.assertEqual(new_nodes[1].text_type, '"code"')
-        self.assertEqual(new_nodes[2].text, '" word"')
-        self.assertEqual(new_nodes[2].text_type, '"text"')
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, "text")
+        self.assertEqual(new_nodes[1].text, "code block")
+        self.assertEqual(new_nodes[1].text_type, "code")
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, "text")
 
     def test_end(self):
         node = TextNode("This is text with a code block *word*", "text")
         new_nodes = nodeutils.split_textnode_delimiter([node], "*", "italic")
-        self.assertEqual(new_nodes[0].text, '"This is text with a code block "')
-        self.assertEqual(new_nodes[0].text_type, '"text"')
-        self.assertEqual(new_nodes[1].text, '"word"')
-        self.assertEqual(new_nodes[1].text_type, '"italic"')
+        self.assertEqual(new_nodes[0].text, "This is text with a code block ")
+        self.assertEqual(new_nodes[0].text_type, "text")
+        self.assertEqual(new_nodes[1].text, "word")
+        self.assertEqual(new_nodes[1].text_type, "italic")
 
     def test_non_text(self):
         node = TextNode("This is text with a code block *word*", "italic")
@@ -40,6 +40,64 @@ class TestNodeUtils(unittest.TestCase):
         node = TextNode("This is text with a **code block word", "text")
         with self.assertRaises(SyntaxError):
             nodeutils.split_textnode_delimiter([node], "**", "bold")
+
+    def test_extract_image(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        result = nodeutils.extract_markdown_images(text)
+        self.assertEqual(result[0][0], "rick roll")
+        self.assertEqual(result[0][1], "https://i.imgur.com/aKaOqIh.gif")
+        self.assertEqual(result[1][0], "obi wan")
+        self.assertEqual(result[1][1], "https://i.imgur.com/fJRm4Vk.jpeg")
+
+    def test_extract_link(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        result = nodeutils.extract_markdown_links(text)
+        self.assertEqual(result[0][0], "to boot dev")
+        self.assertEqual(result[0][1], "https://www.boot.dev")
+        self.assertEqual(result[1][0], "to youtube")
+        self.assertEqual(result[1][1], "https://www.youtube.com/@bootdotdev")
+        
+    def test_split_nodes_image(self):
+        node = TextNode("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)", "text")
+        result = nodeutils.split_nodes_image([node])
+        self.assertEqual(result[0].text, "This is text with a ")
+        self.assertEqual(result[0].text_type, "text")
+        self.assertEqual(result[1].text, "rick roll")
+        self.assertEqual(result[1].text_type, "image")
+        self.assertEqual(result[1].url, "https://i.imgur.com/aKaOqIh.gif")
+        self.assertEqual(result[2].text, " and ")
+        self.assertEqual(result[2].text_type, "text")
+        self.assertEqual(result[3].text, "obi wan")
+        self.assertEqual(result[3].text_type, "image")
+        self.assertEqual(result[3].url, "https://i.imgur.com/fJRm4Vk.jpeg")
+
+    def test_split_nodes_no_image(self):
+        node = TextNode("This is text with a rick roll and obi wan", "text")
+        result = nodeutils.split_nodes_image([node])
+        self.assertEqual(result[0].text, "This is text with a rick roll and obi wan")
+        self.assertEqual(result[0].text_type, "text")
+        self.assertEqual(len(result), 1)
+
+    def test_split_nodes_link(self):
+        node = TextNode("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)", "text")
+        result = nodeutils.split_nodes_link([node])
+        self.assertEqual(result[0].text, "This is text with a link ")
+        self.assertEqual(result[0].text_type, "text")
+        self.assertEqual(result[1].text, "to boot dev")
+        self.assertEqual(result[1].text_type, "link")
+        self.assertEqual(result[1].url, "https://www.boot.dev")
+        self.assertEqual(result[2].text, " and ")
+        self.assertEqual(result[2].text_type, "text")
+        self.assertEqual(result[3].text, "to youtube")
+        self.assertEqual(result[3].text_type, "link")
+        self.assertEqual(result[3].url, "https://www.youtube.com/@bootdotdev")
+
+    def test_split_nodes_no_link(self):
+        node = TextNode("This is text with a link to boot dev and to youtube", "text")
+        result = nodeutils.split_nodes_link([node])
+        self.assertEqual(result[0].text, "This is text with a link to boot dev and to youtube")
+        self.assertEqual(result[0].text_type, "text")
+        self.assertEqual(len(result), 1)
 
 if __name__ == "__main__":
     unittest.main()
