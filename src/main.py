886718f56204540fa36296421daf52d3a007ec27
marvel_magnum <marvel_magnum@yahoo.com>
2024-08-21 17:57:49 -0300

    static content copy

--- src/main.py
+++ src/main.py
@@ -1,16 +1,35 @@
-from textnode import (
-    TextNode,
-    text_type_text,
-    text_type_bold,
-    text_type_italic,
-    text_type_code,
-    text_type_link,
-    text_type_image
-)
+import os
+import shutil
+
+def clean_dir(dir):
+    path = f"{os.getcwd()}/"+ dir
+    print(f"Clearing: {path}...")
+    shutil.rmtree(path, True)
+    os.mkdir(path)
+
+def copy_files(src, dest):
+    items = os.listdir(src)
+    
+    for item in items:
+        src_path = f"{src}/{item}"
+        dest_path = f"{dest}/{item}"
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dest_path)
+            print(f"{src_path}...")
+        else:
+            os.mkdir(dest_path)
+            copy_files(src_path, dest_path)
+
+
+def copy_content(src, dest):
+    src_path = f"{os.getcwd()}/"+ src
+    dest_path = f"{os.getcwd()}/"+ dest
+    print(f"Copying content: {src_path} to {dest_path}...")
+    copy_files(src_path, dest_path)
 
 
 def main():
-    text_node = TextNode("This is a test node", text_type_bold, "https://www.boot.dev")
-    print(text_node)
+    clean_dir("public")
+    copy_content("static", "public")
 
 main()
